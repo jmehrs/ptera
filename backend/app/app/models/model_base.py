580e77ffeea0b482514993d765a8097effe982c6
Jai Mehra <jmjaiken@gmail.com>
2021-10-30 22:26:49 -0400

    Added JSONB db column type

--- backend/app/app/models/model_base.py
+++ backend/app/app/models/model_base.py
@@ -1,14 +1,34 @@
 from typing import Any
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy.sql.schema import MetaData
+from sqlalchemy.engine.default import DefaultDialect
+from sqlalchemy_utils import JSONType
 
-metadata = MetaData(naming_convention={
-    "ix": 'ix_%(column_0_label)s',
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-})
+try:
+    # Use JSONB
+    from sqlalchemy.dialects.postgresql import JSONB
+
+    postgres_jsonb = True
+except:
+    postgres_jsonb = False
+
+
+class JSONBType(JSONType):
+    def load_dialect_impl(self, dialect: DefaultDialect) -> Any:
+        if dialect.name == "postgresql" and postgres_jsonb:
+            return dialect.type_descriptor(JSONB())
+        return super(JSONBType, self).load_dialect_impl(dialect)
+
+
+metadata = MetaData(
+    naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+)
 
 
 @as_declarative(metadata=metadata)
