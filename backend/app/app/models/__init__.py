be173f2947ce0f49c208d0d62af659f5aa37d463
Jai Mehra <jmjaiken@gmail.com>
2021-10-30 22:28:37 -0400

    Replaced old schedule entry with new one

--- backend/app/app/models/__init__.py
+++ backend/app/app/models/__init__.py
@@ -1,4 +1,4 @@
 from .user import User
-from .schedule_entry import ScheduleEntry
+from .canvas_schedule_entry import CanvasScheduleEntry
 from .crontab_schedule import CrontabSchedule
 from .interval_schedule import IntervalSchedule
