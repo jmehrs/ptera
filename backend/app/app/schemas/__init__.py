8770cc86c498177fa8570d6254d11bac6e6b1247
Jai Mehra <jmjaiken@gmail.com>
2021-11-21 22:08:55 -0500

    Updated init with task & canvas shortcuts

--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -1,2 +1,3 @@
-from .task import Task, TaskArgs
 from .user import User, UserCreate, UserInDB, UserUpdate
+from .task import TaskConfig, TaskSignature, TaskID
+from .canvas import Canvas, CanvasCreate, CanvasUpdate
