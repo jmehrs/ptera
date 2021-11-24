ea34f58704f695b6f47edce502160214e3856112
Jai Mehra <jmjaiken@gmail.com>
2021-11-23 21:19:23 -0500

    Refactored CanvasScheduleEntry

--- backend/app/app/models/schedule_entry.py
+++ backend/app/app/models/schedule_entry.py
@@ -1,7 +1,6 @@
 import json
 from typing import Any, Dict
-from celery import signature as signature_
-from celery.canvas import Signature
+
 from celery.schedules import BaseSchedule
 from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.sql.schema import ForeignKey
@@ -10,13 +9,14 @@ from sqlalchemy.orm import relationship
 
 from .crontab_schedule import CrontabSchedule
 from .interval_schedule import IntervalSchedule
-from .model_base import Base, JSONBType
+from .canvas import Canvas
+from .model_base import Base
 
 
-class CanvasScheduleEntry(Base):
+class ScheduleEntry(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
-    signature = Column(JSONBType)
+    signature_id = Column(Integer, ForeignKey("canvas.id"))
     interval_id = Column(Integer, ForeignKey("intervalschedule.id"))
     crontab_id = Column(Integer, ForeignKey("crontabschedule.id"))
     enabled = Column(Boolean, default=True)
@@ -24,17 +24,9 @@ class CanvasScheduleEntry(Base):
     total_run_count = Column(Integer, default=0)
     date_changed = Column(DateTime)
 
+    canvas = relationship(Canvas)
     interval = relationship(IntervalSchedule)
     crontab = relationship(CrontabSchedule)
-
-    @property
-    def sig(self) -> Signature:
-        json_signature = json.loads(self.signature)
-        return signature_(json_signature)
-
-    @sig.setter
-    def sig(self, value: Signature) -> None:
-        self.signature = json.dumps(value)
 
     @property
     def schedule(self) -> BaseSchedule:
