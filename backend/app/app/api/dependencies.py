4f9acb045a628a684252725cbc6b092d7f2e90d3
Jai Mehra <jai.mehra2019@gmail.com>
2021-12-31 01:34:53 +0000

    Cleaned imports & refactored dependency functions

--- backend/app/app/api/dependencies.py
+++ backend/app/app/api/dependencies.py
@@ -1,16 +1,16 @@
-from typing import Generator
+from sqlalchemy.orm.session import Session
 from celery.app.task import Task
 from celery.result import AsyncResult
 
-from fastapi import HTTPException, Path
+from fastapi import HTTPException, Path, Depends
 
 from app.db.session import SessionLocal
-from app.schemas import TaskSignature
-from celery.canvas import Signature
+from app.schemas import Canvas
 from app.core import celery_app
+from app import crud
 
 
-def get_db() -> Generator:
+def get_db() -> Session:
     try:
         db = SessionLocal()
         yield db
@@ -26,16 +26,19 @@ def get_task_info(task_name: str = Path(..., title="Name of the task to get")) -
         return task
 
 
-def get_task_signature(body: TaskSignature) -> Signature:
-
-    if (task := celery_app.tasks.get(body.task)) is None:
-        raise HTTPException(status_code=404, detail=f"Task '{body.task}' not found")
-    else:
-        return Signature(body.dict())
-
-
 def get_task_result(
     task_id: str = Path(..., title="The result id to query")
 ) -> AsyncResult:
     result = AsyncResult(id=task_id)
     return result
+
+
+def get_canvas(
+    canvas_name: str = Path(..., title="The Canvas to retrieve"),
+    db: Session = Depends(get_db),
+) -> Canvas:
+
+    if canvas := crud.canvas.get_by_name(db, canvas_name):
+        return canvas
+    else:
+        raise HTTPException(status_code=404, detail=f"Canvas '{canvas_name}' not found")
