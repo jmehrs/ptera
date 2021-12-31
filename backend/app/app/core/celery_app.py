d59b51f33408399ce46cf5b9d5ce24c7b4408d40
Jai Mehra <jai.mehra2019@gmail.com>
2021-12-31 01:47:00 +0000

    Cleanup & added function to return registered tasks

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -1,10 +1,15 @@
 import os
+from typing import Set
 from app.core.config import settings
 from celery import Celery
 
 
-os.environ.setdefault('CELERY_CONFIG_MODULE', settings.CELERY_CONFIG_MODULE)
+os.environ.setdefault("CELERY_CONFIG_MODULE", settings.CELERY_CONFIG_MODULE)
 
-celery_app = Celery("tasks", include=['app.tasks.tasks'])
-celery_app.config_from_envvar('CELERY_CONFIG_MODULE')
+celery_app = Celery("tasks", include=["app.tasks.tasks"])
+celery_app.config_from_envvar("CELERY_CONFIG_MODULE")
 celery_app.conf.humanize(with_defaults=False, censored=True)
+
+
+def celery_tasks() -> Set:
+    return set(celery_app.tasks)
