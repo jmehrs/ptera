fff19f57481cc4efb9c06d3530197d3fe8d64332
Jai Mehra <jai.mehra2019@gmail.com>
2021-12-31 01:39:31 +0000

    Cleaned imports & added path operations for task and canvas

--- backend/app/app/api/v1/endpoints/run.py
+++ backend/app/app/api/v1/endpoints/run.py
@@ -1,34 +1,30 @@
-from celery import states
-from celery.canvas import Signature
-from celery.result import AsyncResult
-from fastapi import APIRouter, HTTPException, status, Depends
-from app import crud, models, schemas
-from app.core import celery_app
-from app.api.dependencies import get_task_signature
-from app.schemas import TaskSignature
-from typing import Optional, TYPE_CHECKING, Union
-
+from fastapi import APIRouter, status, Depends
+from app import schemas
+from app.utils.celery import apply_async
+from app.api.dependencies import get_canvas
 
 router = APIRouter()
 
 
 @router.post(
-    "/task",
-    summary="Runs the task with given body parameters",
+    "/",
+    summary="Runs an unnamed task signature with given body parameters",
     response_description="A result id of the run task",
     response_model=schemas.TaskID,
     status_code=status.HTTP_201_CREATED,
 )
-def run_task(task_signature: Signature = Depends(get_task_signature)):
-    result = task_signature.apply_async()
+def run_task(body: schemas.TaskSignature):
+    result = apply_async(body.to_signature())
     task_id = schemas.TaskID(id=result.id)
     return task_id
 
 
 @router.post(
-    "/canvas",
-    summary="Runs the canvas with given body parameters",
+    "/{canvas_name}",
+    summary="Runs the canvas with the given name",
     status_code=status.HTTP_201_CREATED,
 )
-def run_canvas():
-    ...
+def run_canvas(canvas: schemas.Canvas = Depends(get_canvas)):
+    result = apply_async(canvas.signature)
+    task_id = schemas.TaskID(id=result.id)
+    return task_id
