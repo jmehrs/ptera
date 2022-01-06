2bca360c1cc4ddddacc0d8567ce268f672fb329d
Jai Mehra <jai.mehra2019@gmail.com>
2022-01-06 19:47:56 +0000

    Removed deprecated --recursive flag

--- backend/app/scripts/format-imports.sh
+++ backend/app/scripts/format-imports.sh
@@ -3,5 +3,5 @@
 set -x
 
 # Sort imports one per line, so autoflake can remove unused imports
-isort --recursive  --force-single-line-imports --apply app
+isort  --force-single-line-imports --apply app
 sh ./scripts/format.sh
