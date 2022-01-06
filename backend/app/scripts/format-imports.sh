941d66b9d5b047a3f8eda44fe159c9620a7f4b42
Jai Mehra <jai.mehra2019@gmail.com>
2022-01-06 19:10:08 +0000

    Added formatting scripts

---
+++ backend/app/scripts/format-imports.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -x
+
+# Sort imports one per line, so autoflake can remove unused imports
+isort --recursive  --force-single-line-imports --apply app
+sh ./scripts/format.sh
