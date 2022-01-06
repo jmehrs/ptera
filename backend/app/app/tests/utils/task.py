85975c3dbd6643fdf022d56eff9b3e516a83f0e8
Jai Mehra <jai.mehra2019@gmail.com>
2022-01-06 03:57:31 +0000

    pep8 & added example tasks

--- backend/app/app/tests/utils/task.py
+++ backend/app/app/tests/utils/task.py
@@ -1,8 +1,68 @@
+from random import choice
+
+from app.core.celery_app import celery_tasks
+
 from celery import signature
 from celery.canvas import Signature
+
 from .utils import random_dict, random_list
-from random import choice
-from app.core.celery_app import celery_tasks
+
+good_bare_minimum_task = {
+    "task": "test.ping",
+}
+
+good_complex_task = {
+    "task": "celery.chain",
+    "subtask_type": "chain",
+    "kwargs": {
+        "tasks": [
+            {
+                "task": "test.echo",
+                "args": ["echo "],
+            },
+            {
+                "task": "test.echo",
+                "args": ["echo "],
+            },
+            {
+                "task": "celery.chord",
+                "subtask_type": "chord",
+                "kwargs": {
+                    "header": [
+                        {
+                            "task": "test.echo",
+                            "args": ["echo "],
+                        },
+                        {
+                            "task": "test.echo",
+                            "args": ["echo "],
+                        },
+                        {
+                            "task": "test.echo",
+                            "args": ["echo "],
+                        },
+                    ],
+                    "body": {
+                        "task": "test.echo",
+                        "args": ["echo "],
+                    },
+                },
+            },
+        ]
+    },
+}
+
+bad_unregistered_task = {
+    "task": "test.non_existant",
+    "options": {"eta": 5, "timeout": 60},
+}
+
+bad_malformed_task = {
+    "oops": "Not even a canvas",
+    "spilled": ["Not", "even", "a", "canvas"],
+    "my": {"macaroni": "with", "the": "nuggets"},
+    "beans": 42,
+}
 
 
 def random_signature() -> Signature:
