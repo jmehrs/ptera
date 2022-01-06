5f4de0c84076e3ac61154e5279e753a838f4c78a
Jai Mehra <jai.mehra2019@gmail.com>
2022-01-06 03:52:48 +0000

    pep8 & added celery fixture

--- backend/app/app/tests/conftest.py
+++ backend/app/app/tests/conftest.py
@@ -1,16 +1,20 @@
-from typing import Dict, Generator
+from typing import Generator
 
 import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy.orm import Session
-
+from app.core.celery_app import celery_app
 from app.db.session import SessionLocal
 from app.main import app
+from fastapi.testclient import TestClient
 
 
 @pytest.fixture(scope="session")
 def db() -> Generator:
     yield SessionLocal()
+
+
+@pytest.fixture(scope="session")
+def celery() -> Generator:
+    yield celery_app
 
 
 @pytest.fixture(scope="module")
