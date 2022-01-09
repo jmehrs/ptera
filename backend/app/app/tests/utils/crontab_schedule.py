d34bd6cb266681e6127b9e73f3ec14a4146f7e9c
Jai Mehra <jai.mehra2019@gmail.com>
2022-01-09 05:29:47 +0000

    Added random schema generation functions

--- backend/app/app/tests/utils/crontab_schedule.py
+++ backend/app/app/tests/utils/crontab_schedule.py
@@ -1,10 +1,9 @@
 import string
 from random import choice
 
-from sqlalchemy.orm import Session
-
 from app import crud, models
 from app.schemas import CrontabScheduleCreate
+from sqlalchemy.orm import Session
 
 
 def random_crontab_interval():
@@ -12,12 +11,16 @@ def random_crontab_interval():
     return choice(interval_choices)
 
 
-def create_random_crontab_schedule(db: Session) -> models.CrontabSchedule:
-    obj_in = CrontabScheduleCreate(
+def random_crontab_schedule_schema() -> CrontabScheduleCreate:
+    return CrontabScheduleCreate(
         minute=random_crontab_interval(),
         hour=random_crontab_interval(),
         day_of_week=random_crontab_interval(),
         day_of_month=random_crontab_interval(),
         month_of_year=random_crontab_interval(),
     )
+
+
+def create_random_crontab_schedule(db: Session) -> models.CrontabSchedule:
+    obj_in = random_crontab_schedule_schema()
     return crud.crontab_schedule.create(db=db, obj_in=obj_in)
--- backend/app/app/tests/utils/interval_schedule.py
+++ backend/app/app/tests/utils/interval_schedule.py
@@ -1,10 +1,9 @@
 from random import choice, randint
 
-from sqlalchemy.orm import Session
-
 from app import crud, models
 from app.schemas import IntervalScheduleCreate
 from app.schemas.interval_schedule import IntervalPeriods
+from sqlalchemy.orm import Session
 
 
 def random_interval_period():
@@ -12,8 +11,10 @@ def random_interval_period():
     return choice(period_choices)
 
 
+def random_interval_schedule_schema() -> IntervalScheduleCreate:
+    return IntervalScheduleCreate(every=randint(0, 9), period=random_interval_period())
+
+
 def create_random_interval_schedule(db: Session) -> models.IntervalSchedule:
-    obj_in = IntervalScheduleCreate(
-        every=randint(0, 9), period=random_interval_period()
-    )
+    obj_in = random_interval_schedule_schema()
     return crud.interval_schedule.create(db=db, obj_in=obj_in)
