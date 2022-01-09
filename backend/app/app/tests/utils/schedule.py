f1af771fecb63622eb0f92788494a2894dfaee36
Jai Mehra <jai.mehra2019@gmail.com>
2022-01-09 05:29:31 +0000

    cleanup

--- backend/app/app/tests/utils/schedule.py
+++ backend/app/app/tests/utils/schedule.py
@@ -1,29 +1,25 @@
 from random import choice
 
+from app import crud, models
+from app.schemas import ScheduleCreate
 from sqlalchemy.orm import Session
 
-from app import crud, models
-from app.models.crontab_schedule import CrontabSchedule
-from app.schemas import ScheduleCreate
-
 from .canvas import create_random_canvas
-from .crontab_schedule import create_random_crontab_schedule
-from .interval_schedule import create_random_interval_schedule
+from .crontab_schedule import random_crontab_schedule_schema
+from .interval_schedule import random_interval_schedule_schema
 from .utils import random_lower_string
 
 
-def create_random_schedule(db: Session) -> models.Schedule:
+def create_random_schedule(db: Session, *, enabled=False) -> models.Schedule:
     name = random_lower_string()
     canvas = create_random_canvas(db=db)
 
     schedule = choice(
-        (create_random_interval_schedule, create_random_crontab_schedule)
-    )(db=db)
+        (random_interval_schedule_schema(), random_crontab_schedule_schema())
+    )
 
-    obj_in = ScheduleCreate(name=name)
+    obj_in = ScheduleCreate(
+        name=name, enabled=enabled, canvas_name=canvas.name, schedule=schedule
+    )
 
-    return (
-        crud.schedule.create_with_crontab
-        if type(schedule) is CrontabSchedule
-        else crud.schedule.create_with_interval
-    )(db=db, obj_in=obj_in, canvas_id=canvas.id, schedule_id=schedule.id)
+    return crud.schedule.create(db=db, obj_in=obj_in)
